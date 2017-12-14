2a1f04b2532c81d03343c46883dd7191ec095dbd
Tucker Schoenthaler <tdschoenthaler@me.com>
2017-12-14 07:45:17 +0000

    Commit Message

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,21 @@
 class HomeController < ApplicationController
   def index
+   
+    if params[:id] == ""
+      @nothing = "Hey, you forgot to enter a symbol"
+    elsif 
+    
+      if params[:id]
+        begin 
+          @stock = StockQuote::Stock.quote(params[:id])
+        rescue StandardError
+          @error = "That's Not A Stock Symbol..."
+        end
+        
+      end
+    end
+    
+    
   end
   
   def about
